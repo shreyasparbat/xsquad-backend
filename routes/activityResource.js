7f89b159118aac0c3924888fbd957668e0da1ca3
Myo <xxyu.mmn.1993@gmail.com>
2018-03-28 17:47:43 +0800

    changed the APIs in activity resource. getActivities retrieves all activity ID and names, and edited getActivityByID

--- routes/activityResource.js
+++ routes/activityResource.js
@@ -4,15 +4,14 @@ var qp = require('flexqp-transaction');
 var moment = require('moment');
 var config = require('../config.json');
 
-router.post('/getActivitiesByCategoryID', async function (req, res, next) {
-    var categoryID = req.body.categoryID;
+router.post('/getActivities', async function (req, res, next) {
 
     try {
-        var result = await qp.executeAndFetchPromise('SELECT activityID, activityName FROM '
-            + config.schema + '.activity where categoryID = ?;', categoryID);
+        var result = await qp.executeAndFetchPromise('SELECT activity_id, activity_name FROM '
+            + config.schema + '.activities;');
 
         if (result.length == 0) {
-            var error = new Error('There are no activities under this categoryID.');
+            var error = new Error('There are no activities.');
             error.status = 406;
             throw error;
         } else {
@@ -28,36 +27,18 @@ router.post('/getActivityByID', async function (req, res, next) {
     var activityID = req.body.activityID;
 
     try {
-        var result = await qp.executeAndFetchPromise('SELECT activityID, activityName,' +
-            'activityDescription, location, minNoOfPeople, maxNoOfPeople, categoryID FROM '
-            + config.schema + '.activity where activityID = ?;', activityID);
+        var result = await qp.executeAndFetchPromise('SELECT activity_id, activity_name,' +
+            'address_line_1, address_line_2, minNoOfPeople, maxNoOfPeople, price_adult, ' +
+            'price_child, category_id FROM '
+            + config.schema + '.activities where activity_id = ?;', activityID);
 
         if (result.length == 0) {
-            var error = new Error('Activity ID does not exist.');
+            var error = new Error('Activity does not exist.');
             error.status = 406;
             throw error;
         } else {
             var activity = result[0];
             res.json(activity);
-
-        }
-    } catch (err) {
-        next(err);
-    }
-});
-
-router.get('/getActivityCategories', async function (req, res, next) {
-    var categories;
-    try {
-        var result = await qp.executeAndFetchPromise('SELECT categoryID, categoryName from '
-            + config.schema + '.category');
-
-        if (result.length == 0) {
-            var error = new Error('There are no categories.');
-            error.status = 406;
-            throw error;
-        } else {
-            res.json(result);
 
         }
     } catch (err) {
